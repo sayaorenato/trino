cffb7de3d62dae03e7d3f41933b75d5db36edf5f
sayaorenato <sayaorenato@github.com>
2026-08-14 23:17:59 -0300

    feat: suporte a PWA e icone customizado para atalho na tela inicial

--- app/+html.tsx
+++ app/+html.tsx
@@ -10,9 +10,17 @@ export default function Root({ children }: PropsWithChildren) {
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <title>Trino — Fé em Constância</title>
 
+        {/* PWA & iOS Safari Home Screen Meta Tags */}
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="Trino" />
+        <meta name="theme-color" content="#03192E" />
+
         {/* Favicon & Mobile Home Screen Launcher Icons */}
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
         <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/icon.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="apple-touch-icon-precomposed" href="/apple-touch-icon-precomposed.png" />
         <link rel="manifest" href="/manifest.json" />
